f32ee76c7e9ad8bde6f9a847e4b23141fd435013
romamaxime <roma.maxime1994@gmail.com>
2017-05-09 17:03:54 +0200

    amélioration du formulaire et ajout de design

--- public/app.js
+++ public/app.js
@@ -1,6 +1,38 @@
 $(document).ready(function() 
 {
 
+
+	// function inscription(){
+	// 	$.ajax(
+	// 	{
+	// 		url : 'http:localhost:3000/inscription',
+	// 		type : 'post',
+	// 		data : 
+	// 		{
+	// 			prenom : $('#prenom').val(),
+	// 			nom : $('#nom').val(), 
+	// 			email : $('#email').val(),
+	// 			mdp : $('#mdp').val(),
+	// 			date : $('#jour').val() + "/" + $('#mois').val() + "/" + $('#annee').val(),
+	// 			genre : $('genre').prop('checked'),
+
+	// 		}
+
+	// 	}).done(function(data)
+	// 	{
+	// 		console.log(data);
+	// 	});
+	// };
+
+	$('#buttonInscription').on('submit', function(e)
+	{
+	 	e.preventDefault();
+	 	
+	});
+
+
+
+	// Pour activer les sélects
 	$('select').material_select();
 
 });
---
+++ server.js
@@ -0,0 +1,30 @@
+//-- Web Server
+
+var express = require('express');
+var app = express();
+app.listen(3000);
+var expressValidator = require('express-validator');
+var bodyParser = require('body-parser');
+
+//-Middleware
+
+app.use(bodyParser.urlencoded({extended : false}));
+app.use(expressValidator());
+
+
+//--Pages
+
+function inscriptionServer(req, resp)
+{
+	console.log(req.body);
+	resp.send('ok');
+}
+
+
+//--Routes
+
+app.use(express.static('public'));
+
+
+app.post('/inscription', inscriptionServer);
+
